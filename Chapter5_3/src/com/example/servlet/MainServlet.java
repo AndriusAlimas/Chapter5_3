b6ad7b3687f9f4b20e389d28c0521178a668d210
Andrius <andriusjavait@gmail.com>
2017-01-15 20:43:53 +0000

    Servlet class update to test if it working listener class

--- Chapter5_3/src/com/example/servlet/MainServlet.java
+++ Chapter5_3/src/com/example/servlet/MainServlet.java
@@ -1,6 +1,8 @@
 package com.example.servlet;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import com.example.Dog;
+
 import java.io.*;
 
 public class MainServlet extends HttpServlet {
@@ -11,7 +13,10 @@ public class MainServlet extends HttpServlet {
         out.println("<html>");
         out.println("<head><title>Test ServletContextListener </title></head>");
         out.println("<body>test context attributes set by listener<br>");
-        
+        // Now get the Dog from ServletContext. If the listener method worked the Dog will be there BEFORE
+        // this service method is called for the first time:
+        Dog dog = (Dog)getServletContext().getAttribute("dog");
+        out.println("<hr><b>Dog's breed is: " + dog.getBreed() + "</b><br>");
         out.println("</body></html>");
 	}
 }
