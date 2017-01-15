9b69ca98c26fe58f632a57c350e2263129c3caf7
Andrius <andriusjavait@gmail.com>
2017-01-15 20:10:32 +0000

    created plain listener class for now do nothing

---
+++ Chapter5_3/src/com/example/listener/MyServletContextListener.java
@@ -0,0 +1,19 @@
+package com.example.listener;
+import com.example.Dog;
+import javax.servlet.*;
+
+public class MyServletContextListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent ContextEvent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent ContextEvent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
