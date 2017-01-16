2288c64f856951efd2c9074c02aef5199c55c491
Andrius <andriusjavait@gmail.com>
2017-01-16 14:34:17 +0000

    update servlet doGet() method with couple methods from servletContext with explanation

--- Chapter5_3/src/com/example/servlet/MainServlet.java
+++ Chapter5_3/src/com/example/servlet/MainServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.*;
 import com.example.Dog;
 
 import java.io.*;
+import java.util.Enumeration;
 
 public class MainServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request,
@@ -17,6 +18,23 @@ public class MainServlet extends HttpServlet {
         // this service method is called for the first time:
         Dog dog = (Dog)getServletContext().getAttribute("dog");
         out.println("<hr><b>Dog's breed is: " + dog.getBreed() + "</b><br>");
+        out.println("<hr><b>Now Testing getAttributeNames() all Enumeration list:</b><br>");
+        
+        Enumeration enumeration = this.getServletContext().getAttributeNames();
+        while (enumeration.hasMoreElements()) {
+            out.println("<br><i>" + enumeration.nextElement() + "</i>");
+        }
+        // int getMajorVersion() returns the major version of the Servlet API that this servlet 
+        // container supports. All implementations that comply with Version 3.0 must have this method return the integer 3.
+        out.println("<hr>Server Version: <i><b>" + this.getServletContext().getMajorVersion() + "</i></b>");
+        
+        // java.lang.String getServerInfo() returns the name and version of the servlet container on
+        // which the servlet is running. The form of the returned string is servername/versionnumber.
+        // For example, the JavaServer Web Development Kit may return the string JavaServer Web Dev Kit/1.0.
+        // The servlet container may return other optional information after the primary string in 
+        // parentheses, for example, JavaServer Web Dev Kit/1.0 (JDK 1.1.6; Windows NT 4.0 x86).
+        out.println("<hr>Server Info: <i><b>" + this.getServletContext().getServerInfo() + "</i></b>");
+        
         out.println("</body></html>");
 	}
 }
